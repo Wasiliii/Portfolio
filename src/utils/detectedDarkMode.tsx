cb68dca39a53f046124fa73745c9d1c77c36bfbb
wasya <wasis1999@gmail.com>
2023-05-28 17:19:31 +0300

    почти все готово осталось чутка шлифовки иконки кнопки и свои контакты забить

---
+++ src/utils/detectedDarkMode.tsx
@@ -0,0 +1,8 @@
+
+const detectedDarkMode = ()=>{
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+      return "dark"
+    }
+    return "light"
+}
+export default  detectedDarkMode
